9eadc692aa0410a67be44e55f7dfdcbe4998d67e
Chris W <chriswxyz@outlook.com>
2016-10-05 09:43:35 -0500

    Export fields

--- bin/index.d.ts
+++ bin/index.d.ts
@@ -2,5 +2,7 @@ export { Button } from './button/button';
 export { Code } from './code/code';
 export { Modal } from './modal/modal';
 export { Form } from './form/form';
+export { NumberField } from './form/number-field';
+export { TextField } from './form/text-field';
 import * as ToastSvc from './toast/toast-service';
 export declare const Toast: typeof ToastSvc;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export { Button } from './button/button';
 export { Code } from './code/code';
 export { Modal } from './modal/modal';
 export { Form } from './form/form';
+export { NumberField } from './form/number-field';
+export { TextField } from './form/text-field';
 
 import * as ToastSvc from './toast/toast-service';
 export const Toast = ToastSvc;
