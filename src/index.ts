67313be9c614eaf11ac71770841c26546398a7fa
Chris W <chriswxyz@outlook.com>
2016-10-05 13:27:02 -0500

    Export select fields

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ export { Modal } from './modal/modal';
 export { Form } from './form/form';
 export { NumberField } from './form/number-field';
 export { TextField } from './form/text-field';
+export { Select } from './form/select';
+export { SearchNSelect } from './form/search-n-select';
 
 import * as ToastSvc from './toast/toast-service';
 export const Toast = ToastSvc;
