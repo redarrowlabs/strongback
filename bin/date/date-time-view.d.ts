b9ce843057a761027a0315c1ce8e67c76c3cb88d
Chris Williams <chwilliams@redarrowlabs.com>
2017-04-03 15:09:11 -0500

    prune bin

--- bin/date/date-time-view.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-export interface DateTimeViewProps {
-    date: string;
-    pattern?: string;
-}
-export declare function DateTimeView(props: DateTimeViewProps): JSX.Element;
