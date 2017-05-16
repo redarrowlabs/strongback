bf5c73123a071b0b32d49c00cedbe4939fc5e5d1
Chris Williams <chwilliams@redarrowlabs.com>
2017-05-16 13:35:44 -0500

    Add a multiline text option

--- bin/form/text-field.d.ts
+++ bin/form/text-field.d.ts
@@ -3,9 +3,11 @@ import { IField, IFieldComponent } from './fields';
 export interface TextFieldStatelessProps extends IFieldComponent<string> {
     autoComplete: 'on' | 'off';
     label: string;
+    multiline?: boolean;
 }
 export declare function TextFieldStateless(props: TextFieldStatelessProps): JSX.Element;
 export interface TextFieldProps extends IField {
     autoComplete?: 'on' | 'off';
+    multiline?: boolean;
 }
 export declare function TextField(props: TextFieldProps): JSX.Element;
--- bin/form/text-field.js
+++ bin/form/text-field.js
@@ -4,13 +4,15 @@ var React = require("react");
 var redux_form_1 = require("redux-form");
 var field_wrapper_1 = require("./field-wrapper");
 function TextFieldStateless(props) {
-    var _a = props.input, value = _a.value, onChange = _a.onChange, onBlur = _a.onBlur, onFocus = _a.onFocus, autoComplete = props.autoComplete;
-    return React.createElement(field_wrapper_1.FieldWrapper, { fieldProps: props },
-        React.createElement("input", { type: 'text', value: value, onChange: onChange, onBlur: onBlur, onFocus: onFocus, autoComplete: autoComplete }));
+    var _a = props.input, value = _a.value, onChange = _a.onChange, onBlur = _a.onBlur, onFocus = _a.onFocus, autoComplete = props.autoComplete, multiline = props.multiline;
+    var control = multiline
+        ? React.createElement("textarea", { value: value, onChange: onChange, onBlur: onBlur, onFocus: onFocus })
+        : React.createElement("input", { type: 'text', value: value, onChange: onChange, onBlur: onBlur, onFocus: onFocus, autoComplete: autoComplete });
+    return React.createElement(field_wrapper_1.FieldWrapper, { fieldProps: props }, control);
 }
 exports.TextFieldStateless = TextFieldStateless;
 function TextField(props) {
-    return React.createElement(redux_form_1.Field, { name: props.name, component: TextFieldStateless, autoComplete: props.autoComplete || 'off', label: props.label, onBlur: props.onBlur });
+    return React.createElement(redux_form_1.Field, { name: props.name, component: TextFieldStateless, autoComplete: props.autoComplete || 'off', label: props.label, onBlur: props.onBlur, multiline: props.multiline });
 }
 exports.TextField = TextField;
 //# sourceMappingURL=text-field.js.map
