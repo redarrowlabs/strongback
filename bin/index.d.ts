0cb4612934dec60bc5d84ab9842cc64432b2097d
Chris Williams <chwilliams@redarrowlabs.com>
2017-09-14 09:44:14 -0500

    Version build lint

--- bin/index.d.ts
+++ bin/index.d.ts
@@ -1,8 +1,7 @@
 export { Button } from './button/button';
 export { Modal } from './modal/modal';
 export * from './form';
-export { LocalDateView } from './date/local-date-view';
-export { LocalDateTimeView } from './date/local-date-time-view';
+export * from './date';
 export { useDefaultImplementations } from './defaults';
 export { setLoader } from './loader/loader';
 import * as ToastSvc from './toast/toast-service';
